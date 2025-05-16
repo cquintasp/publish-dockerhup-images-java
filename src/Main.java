ffd93acca1a9a6a07cb91286be0622b73b3cbd83
Chandra quintas <cquintasp@ultrasist.com.mx>
2025-05-16 15:22:46 -0600

    actualizado

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World actualizado");
+        System.out.println("Hello World actualizado a");
     }
 }
